0508fd0cd3517ddf21cf112322d538a6b1cf9fe9
Young Lad <gabrieljohnsoniv@gmail.com>
2016-07-07 15:18:46 -0400

    refactored routes using resources

--- Week4/timetracking/config/routes.rb
+++ Week4/timetracking/config/routes.rb
@@ -1,27 +1,8 @@
 Rails.application.routes.draw do
   get '/', to: 'site#home'
 
-  get '/project', to: 'projects#index'
-
-  get '/project/:id', to:  'projects#show'
-  
-  get '/project/new', to: 'projects#new'
-
-  post '/project', to: 'projects#create'
-
-  #Time entry routes
-
-  get '/project/:project_id/time_entries', to: 'time_entries#index' 
-
-  get '/project/:project_id/time_entries/new', to: 'time_entries#new'
-
-  post "project/:project_id/time_entries", to: "time_entries#create", as: :project_time_entries
-
-  get '/project/:project_id/time_entries/:id/edit', to: 'time_entries#edit'
-
-  patch "/project/:project_id/time_entries/:id", to: "time_entries#update", as: "project_time_entry"
-
-  delete "/project/:project_id/time_entries/:id", to: "time_entries#destroy"
-
+  resources :projects, only: [:index, :show, :new, :create] do
+  	resources :time_entries, except: [:show]
+  end
 
 end
